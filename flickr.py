71e8baeef70ac74a0f8a3323b175125d2396b68f
ltsheridan <31678807+ltsheridan@users.noreply.github.com>
2017-10-26 16:40:59 -0400

    modified function

--- flickr.py
+++ flickr.py
@@ -25,7 +25,7 @@ def params_unique_combination(baseurl, params_d, private_keys=["api_key"]):
             res.append("{}-{}".format(k, params_d[k]))
     return baseurl + "_".join(res)
 
-def search_flickr_by_tags(tags):
+def search_flickr(key):
     if not FLICKR_API_KEY:
         raise Exception('Flickr API Key is missing!')
 
@@ -34,7 +34,7 @@ def search_flickr_by_tags(tags):
         "method": "flickr.photos.search",
         "format": "json",
         "api_key": FLICKR_API_KEY,
-        "tags": tags,
+        "tags": key,
         "per_page": 10,
         "nojsoncallback": 1
     }
